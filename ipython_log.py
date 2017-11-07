ae31a40acce6595c8e95a20ba18eabd388094a8c
Scott G. Daniel <scottdaniel25@gmail.com>
2017-11-07 16:39:30 -0700

    getting there

---
+++ ipython_log.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'ls test_phaster_out/')
+args['outPath']
+args['outPath']+jobid+'.zip'
+args['input']
+import lzma
+import zip
+fileName=args['input']
+fileName.trim
+fileName.split('.')
+fileName.split('/')
+fileName.split('/')[1]
+fileName.split('/')[1].replace('fna','zip')
+get_ipython().magic(u'pinfo unzip')
+get_ipython().magic(u'pinfo zip')
+get_ipython().magic(u'save ')
+get_ipython().magic(u'pinfo save')
+get_ipython().magic(u'save ipython_log.py 1-18')
